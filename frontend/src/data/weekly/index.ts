6072f06886537bb962f723884604f9987c22531d
Zeze(강재영) <wooder2050@gmail.com>
2026-03-15 14:06:06 +0900

    feat: 2025년 9월 주간 국회 뉴스 4주차 추가
    - w1: 정기회 개회, 위원회 35회
    - w2: 특검법 3건 통과 (김건희·순직해병·내란)
    - w3: 정부조직법 법사위 본격 심사
    - w4: 검찰청 폐지 정부조직법 등 13건 표결, 4박5일 필리버스터

--- frontend/src/data/weekly/index.ts
+++ frontend/src/data/weekly/index.ts
@@ -1,4 +1,8 @@
 import type { WeeklyArticle } from "./types";
+import w202509w1 from "./2025-09-w1";
+import w202509w2 from "./2025-09-w2";
+import w202509w3 from "./2025-09-w3";
+import w202509w4 from "./2025-09-w4";
 import w202510w1 from "./2025-10-w1";
 import w202510w2 from "./2025-10-w2";
 import w202510w3 from "./2025-10-w3";
@@ -46,6 +50,10 @@ const articles: WeeklyArticle[] = [
   w202510w3,
   w202510w2,
   w202510w1,
+  w202509w4,
+  w202509w3,
+  w202509w2,
+  w202509w1,
 ];
 
 export function getAllWeeklyArticles(): WeeklyArticle[] {
